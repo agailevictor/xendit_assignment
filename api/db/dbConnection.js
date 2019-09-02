4f7111daadd92643e714474be36338680cb0f931
Agaile Victor <agaile.victor@baesystems.com>
2019-09-02 16:40:47 +0800

    docker modifications

--- api/db/dbConnection.js
+++ api/db/dbConnection.js
@@ -1,12 +1,12 @@
 import dotenv from "dotenv";
 import pg from "pg";
 dotenv.config();
-
 const pool = new pg.Pool({
-    user: process.env.PG_ROOT_USER,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_ROOT_PASSWORD,
-    port: process.env.PG_PORT,
+	host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     max: 10,
     idleTimeoutMillis: 30000,
 });
